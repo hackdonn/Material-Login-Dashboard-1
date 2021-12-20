15a3aacaf2b663865c455abf304755c7dbf0f9d2
DivyeshBoda73 <divyesh.boda.jc@joshsoftware.com>
2021-12-20 10:38:43 +0530

    This is initial as well as final commit for this project.

---
+++ app/src/main/java/com/hackdon/dash1/LoginActivity.java
@@ -0,0 +1,27 @@
+package com.hackdon.dash1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        TextView signUp = findViewById(R.id.textView6);
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent n = new Intent(LoginActivity.this, SignUpActivity.class);
+                startActivity(n);
+            }
+        });
+
+    }
+}
